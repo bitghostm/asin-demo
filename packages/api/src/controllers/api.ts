f9d5964ab930a8efc67ec4a7aeea059d9860dc9f
make711 <marco91414@gmail.com>
2019-09-06 19:56:59 -0700

    parse html string to dom

--- packages/api/src/controllers/api.ts
+++ packages/api/src/controllers/api.ts
@@ -3,19 +3,45 @@
 import { Response, Request, NextFunction } from "express";
 import puppeteer from "puppeteer";
 import xpath from "xpath";
-import { DOMParser } from "xmldom";
 import request from "request-promise";
+import parse5 from 'parse5';
+import xmlser from 'xmlserializer';
+import xmldom, { DOMParser } from 'xmldom';
 
-/**
- * GET /api
- * List of API examples.
- */
 export const getApi = async (req: Request, res: Response) => {
     console.time("request");
-    const result = await request.get("https://www.amazon.com/dp/B002QYW8LW");
+    const result = await request.get({ uri: "https://www.amazon.com/dp/B002QYW8LW", 
+    headers: {
+        "ContentType": "text/html", 
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Host': 'www.amazon.com',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+    },
+    gzip: true
+    });
     console.timeEnd("request");
-    console.log(result.match(/Product Dimensions/g));
-    // const doc = new DOMParser().parseFromString(result);
+    
+    console.time("parse");
+    // parse html string to DOM
+    const document = parse5.parse(result);
+    const xhtml = xmlser.serializeToString(document);
+    const doc = new DOMParser().parseFromString(xhtml);
+    const select = xpath.useNamespaces({"x": "http://www.w3.org/1999/xhtml"});
+
+    // const name = xpath.select("//td[contains(text(), 'Product Dimensions')]/following-sibling::td", doc);
+    const name = select("//x:td[contains(text(), 'Product Dimensions')]/following-sibling::td[1]", doc);
+    console.log('\n\n', name);
+    console.timeEnd("parse");
+
+
+
+
+    // console.log(result.match(/Product Dimensions/g));
     // console.log(xpath.select('//body'));
     // console.time('launchBrowser');
     // const browser = await puppeteer.launch();
