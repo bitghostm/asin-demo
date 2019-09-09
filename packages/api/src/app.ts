c619c21a2967d08af44471230b8300be264e8d0b
make711 <marco91414@gmail.com>
2019-09-09 10:41:26 -0700

    switch parser and save search product info

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import compression from "compression";  // compresses requests
 import bodyParser from "body-parser";
-import lusca from "lusca";
 import mongoose from "mongoose";
 import bluebird from "bluebird";
+import cors from 'cors';
 import { MONGODB_URI } from "./util/secrets";
 
 // Controllers (route handlers)
@@ -28,11 +28,11 @@ app.set("port", process.env.PORT || 3000);
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(lusca.xframe("*"));
-app.use(lusca.xssProtection(true));
+app.use(cors({ origin: '*' }));
 
 // app routes
-app.get("/", apiController.getApi);
-app.get("/api", apiController.getApi);
+app.get("/", apiController.listProduct);
+app.get("/product/insert/:asin", apiController.insertProduct);
+app.get("/product/list", apiController.listProduct);
 
 export default app;
--- packages/api/src/controllers/api.ts
+++ packages/api/src/controllers/api.ts
@@ -1,70 +1,54 @@
 "use strict";
 
 import { Response, Request, NextFunction } from "express";
-import puppeteer from "puppeteer";
-import xpath from "xpath";
-import request from "request-promise";
-import parse5 from 'parse5';
-import xmlser from 'xmlserializer';
-import xmldom, { DOMParser } from 'xmldom';
+import osmosis from 'osmosis';
+import { Product } from '../models/Product';
 
-export const getApi = async (req: Request, res: Response) => {
-    console.time("request");
-    const result = await request.get({ uri: "https://www.amazon.com/dp/B002QYW8LW", 
-    headers: {
-        "ContentType": "text/html", 
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Host': 'www.amazon.com',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-    },
-    gzip: true
-    });
-    console.timeEnd("request");
-    
-    console.time("parse");
-    // parse html string to DOM
-    const document = parse5.parse(result);
-    const xhtml = xmlser.serializeToString(document);
-    const doc = new DOMParser().parseFromString(xhtml);
-    const select = xpath.useNamespaces({"x": "http://www.w3.org/1999/xhtml"});
+export const selectors = {
+  rank: [
+    "//td[contains(text(), 'Best Sellers')]/following-sibling::td/text()",
+    "//b[contains(text(), 'Best Sellers')]/following-sibling::text()",
+  ].join('|'),
+  dimensions: "//td[contains(text(), 'Product Dimensions')]/following-sibling::td",
+  category: "div#wayfinding-breadcrumbs_container li a:first-of-type",
+}
 
-    // const name = xpath.select("//td[contains(text(), 'Product Dimensions')]/following-sibling::td", doc);
-    const name = select("//x:td[contains(text(), 'Product Dimensions')]/following-sibling::td[1]", doc);
-    console.log('\n\n', name);
-    console.timeEnd("parse");
+export type Result = {
+  [key in keyof typeof selectors]: string;
+}
 
+function cleanResult(data: Result): Result {
+  return {
+    ...data,
+    rank: data.rank && data.rank.replace(/ \(/g, '')
+  }
+}
 
+enum ResponseStatuses {
+  created = 'created',
+  updated = 'updated',
+}
 
-
-    // console.log(result.match(/Product Dimensions/g));
-    // console.log(xpath.select('//body'));
-    // console.time('launchBrowser');
-    // const browser = await puppeteer.launch();
-    // const page = await browser.newPage();
-    // await page.setRequestInterception(true);
-
-    // page.on('request', (req) => {
-    //     if(req.resourceType() === 'image'){
-    //         req.abort();
-    //     }
-    //     else {
-    //         req.continue();
-    //     }
-    // });
-    // console.timeEnd('launchBrowser');
-    // console.time('pageLoad');
-    // await page.goto("https://www.amazon.com/dp/B002QYW8LW");
-    // // await page.goto('view-source:https://www.amazon.com/dp/B002QYW8LW', {waitUntil: 'networkidle2'})
-    // console.timeEnd('pageLoad');
-    // const itemWeightNode = await page.$x("//td[contains(text(), 'Product Dimensions')]/following-sibling::td");
-
-    // const itemWeight = await page.evaluate(element => element.textContent, itemWeightNode[0]);
-
-    // res.status(200).send({ "test": itemWeight });
-    res.status(200).send({ test: result });
+export const insertProduct = async (req: Request, res: Response) => {
+  const asin = req.params.asin;
+  return osmosis.get(`https://www.amazon.com/dp/${asin}`)
+    .set(selectors)
+    .data(async (result: Result) => {
+      const data = { asin, ...cleanResult(result) };
+      const existingProduct = await Product.findOne({ asin }).exec();
+      if (existingProduct) {
+        Object.assign(existingProduct, data);
+        await existingProduct.save();
+        return res.status(200).send({ status: ResponseStatuses.updated, data });
+      } else {
+        const product = new Product(data);
+        await product.save();
+        return res.status(200).send({ status: ResponseStatuses.created, data });
+      }
+    });;
 };
+
+export const listProduct = async (req: Request, res: Response) => {
+  const data = await Product.find();
+  return res.status(200).send({ data });
+}
